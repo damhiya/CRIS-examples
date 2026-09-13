6883826abee0e895ded72a5542cfd32b48d91f96
damhiya <damhiya@gmail.com>
2026-09-13 20:41:24 +0900

    sync with CRIS

--- flake.nix
+++ flake.nix
@@ -2,45 +2,52 @@
   description = "CRIS development environment";
   inputs = {
     nixpkgs.url = "github:snu-sf/nixpkgs/rocq-env-0.2";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        coq = pkgs.coq_9_0;
-        coqPackages = pkgs.mkCoqPackages coq;
-        callPackage = pkgs.lib.callPackageWith (pkgs // params // set);
-        params = {
-          inherit (coqPackages) mkCoqDerivation lib stdlib;
-          inherit (coq) ocamlPackages;
-        };
-        set = {
-          inherit coq;
-          paco = callPackage coqPackages.paco.override { version = "4.2.3"; };
-          ExtLib = callPackage coqPackages.ExtLib.override { version = "0.13.0"; };
-          ITree = callPackage coqPackages.ITree.override { version = "5.2.1"; };
-          Ordinal = callPackage coqPackages.Ordinal.override { version = "0.5.6"; };
-          stdpp = callPackage coqPackages.stdpp.override { version = "1.12.0"; };
-          iris = callPackage coqPackages.iris.override { version = "4.4.0"; };
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = pkgs.lib.attrValues set ++ [
-            coqPackages.coq-lsp
-            pkgs.coqtail-mcp
-          ];
-        };
-      }
-    );
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          coq = pkgs.coq_9_0;
+          coqPackages = pkgs.mkCoqPackages coq;
+          callPackage = pkgs.lib.callPackageWith (pkgs // params // set);
+          params = {
+            inherit (coqPackages) mkCoqDerivation lib stdlib;
+            inherit (coq) ocamlPackages;
+          };
+          set = {
+            inherit coq;
+            paco = callPackage coqPackages.paco.override { version = "4.2.3"; };
+            ExtLib = callPackage coqPackages.ExtLib.override { version = "0.13.0"; };
+            ITree = callPackage coqPackages.ITree.override { version = "5.2.1"; };
+            Ordinal = callPackage coqPackages.Ordinal.override { version = "0.5.6"; };
+            stdpp = callPackage coqPackages.stdpp.override { version = "1.12.0"; };
+            iris = callPackage coqPackages.iris.override { version = "4.4.0"; };
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = pkgs.lib.attrValues set ++ [
+              coqPackages.coq-lsp
+              pkgs.coqtail-mcp
+            ];
+            shellHook = ''
+              unset COQPATH
+            '';
+          };
+        }
+      );
+    };
 }
